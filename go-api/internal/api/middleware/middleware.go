4361cdc502b959d7b0e6f1d59aeb1045b5f9f098
basilysf1709 <basilyusuf1709@gmail.com>
2024-01-31 06:31:00 -0500

    Added Token Bucket Algorithm

--- go-api/internal/api/middleware/middleware.go
+++ go-api/internal/api/middleware/middleware.go
@@ -4,6 +4,9 @@ import (
     "log"
     "net/http"
     "time"
+    "go-api/internal/services/nameservice"
+    "encoding/json"
+    "strconv"
 )
 
 
@@ -16,4 +19,41 @@ func LoggingMiddleware(next http.Handler) http.Handler {
         next.ServeHTTP(w, r)
         log.Printf("Completed in %v", time.Since(start))
     })
+}
+
+
+type ErrorResponse struct {
+    Error       string `json:"error"`
+    Description string `json:"description"`
+    Status      int    `json:"status"`
+}
+
+func RateLimitMiddleware(tb *nameservice.TokenBucket) func(http.Handler) http.Handler {
+    return func(next http.Handler) http.Handler {
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            if !tb.Allow() {
+                // Create an ErrorResponse struct
+                timeLeft := tb.SecondsUntilNextRequest()
+                errResp := ErrorResponse{
+                    Error:       "Too Many Requests",
+                    Description: "You have exceeded the rate limit. Please wait for " + strconv.Itoa(timeLeft) +  " seconds before making another request.",
+                    Status:      http.StatusTooManyRequests,
+                }
+
+                // Encode the ErrorResponse to JSON
+                jsonResponse, err := json.Marshal(errResp)
+                if err != nil {
+                    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+                    return
+                }
+
+                // Send the JSON response with HTTP 429 status
+                w.Header().Set("Content-Type", "application/json")
+                w.WriteHeader(http.StatusTooManyRequests)
+                w.Write(jsonResponse)
+                return
+            }
+            next.ServeHTTP(w, r)
+        })
+    }
 }
